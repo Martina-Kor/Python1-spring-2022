4d98a41a24b6fa4f236078bef3652f94e4af8d26
Martina Korandova <martina.maserkap8@gmail.com>
2022-04-25 20:09:36 +0200

    Šestý úkol, v1

--- 6hw_car_rental.py
+++ 6hw_car_rental.py
@@ -12,21 +12,15 @@ Krom metod str (převod na řetězec) a int (převod na celé číslo) existuje 
 Bonus:
 Upravte váš program tak, aby jméno souboru k otevření zadal uživatel, abychom mohli takto zpracovávat výkazy z různých souborů, aniž bychom museli upravovat samotný kód programu. Program ověřte tak, že si soubor auta.txt přejmenujete, nebo si vytvořte nový. """
 
-with open('Python1-spring-2022/auta.txt', encoding='utf-8') as vstup:
-  zaznamy = vstup.readlines()
-  
-print(zaznamy)
 
-radky = [zaznam.strip() for zaznam in zaznamy]
-print(radky)
+filename = input('Zadejte nazev souboru: ')
 
-radky = [zaznam.split() for zaznam in zaznamy]
-print(radky)
+with open('Python1-spring-2022/' + filename) as vstup:
+    auta = vstup.readlines()
+auta = [radek.split() for radek in auta]
 
-radky_oprava = [[zaznam[0], (float(zaznam[1].replace(",", ".")))] for zaznam in radky]
-print(radky_oprava)
+km = [float(auto[1].replace(',', '.')) for auto in auta]
 
-""" celkovy_pocet_km = [sum(zaznam[1]) for zaznam in radky_oprava]
-print(celkovy_pocet_km)
+celkem_km = sum(km)
 
- """
+print(celkem_km)
